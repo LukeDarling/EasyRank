45137f924e4c662ba0c1980d28e52fc7787eab59
Luke Darling <HTMLguy7@gmail.com>
2014-07-23 15:22:38 -0400

    Update Main.php

--- src/LDX/EasyRank/Main.php
+++ src/LDX/EasyRank/Main.php
@@ -1,1 +1,28 @@
-null
+<?php
+namespace LDX\EasyRank;
+use pocketmine\command\Command;
+use pocketmine\command\CommandSender;
+use pocketmine\Player;
+use pocketmine\plugin\PluginBase;
+use pocketmine\Server;
+use pocketmine\utils\TextFormat as Color;
+use pocketmine\event\PlayerCommandPreprocessEvent as CommandEvent;
+class Main extends PluginBase {
+  public function onLoad() {
+    $this->getLogger()->info(TextFormat::YELLOW . "Loading EasyRank v" . $this->getDescription()->getVersion() . " by LDX...");
+  }
+  public function onEnable() {
+    if(!file_exists($this->getDataFolder() . "config.yml")) {
+      @mkdir($this->getDataFolder());
+      file_put_contents($this->getDataFolder() . "config.yml",$this->getResource("config.yml"));
+    }
+    $this->getLogger()->info(TextFormat::YELLOW . "Enabling EasyRank...");
+  }
+  public function onCommand(CommandSender $issuer,Command $cmd,$label,array $args) {
+    
+  }
+  public function onDisable() {
+    $this->getLogger()->info(TextFormat::YELLOW . "Disabling EasyRank...");
+  }
+}
+?>
